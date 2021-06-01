7b601b32a3dfcaaaa4de1e140fb93910ae8c0466
Procoder16 <smsoumik1608@gmail.com>
2021-06-01 10:45:11 +0530

    added the doubly linked list program with the node structure of doubly linked list

---
+++ doublyLinkedList.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* prev;
+
+    node(int val){
+        data = val;
+        next = NULL:
+        prev = NULL;
+    }
+};
+
+int main(){
+
+}
